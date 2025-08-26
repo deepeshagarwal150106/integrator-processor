4626d404a18d2704c96b5f7aabb2c3e0fcc70b32
deepesh agarwal <deepeshagarwal150106@gmail.com>
2025-08-26 08:37:52 +0530

    processor for integrator integrated

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
+
+const TOPIC_NAME = "zap-events";
+
+const client = new PrismaClient();
+
+const kafka = new Kafka({
+  clientId: "outboc-processor",
+  brokers: ["localhost:9092"], 
+});
+
+async function main() {
+  try {
+    const producer = kafka.producer();
+    await producer.connect();
+    while (true) { 
+      const pendingRows = await client.zapRunOutBox.findMany({
+        where: {},
+        take: 10
+      });
+      producer.send({
+        topic: TOPIC_NAME,
+        messages: pendingRows.map(row => {
+          return {
+            value: JSON.stringify({ zapRunId: row.zapRunId, stage: 0 }),
+            key: row.id.toString()
+          };
+        })
+      });
+      await client.zapRunOutBox.deleteMany({
+        where: {
+            id: {
+                in: pendingRows.map(row => row.id)
+            }
+        }
+      });
+    }
+  } catch (error) {
+    console.error("Error in processor:", error);
+  }
+
+}
+main().catch(console.error);
